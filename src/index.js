48f4e6226079b62a7bdc17c79e23e5c432d41109
unknown <05vp94@mail.ru>
2021-08-29 18:53:22 +0300

    fix gh pages

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, Switch, Route, useLocation} from "react-router-dom"
+import {BrowserRouter as Router, Switch, Route, useLocation} from "react-router-dom";
 import Home from './componets/pages/Home';
 import { BiHome } from 'react-icons/bi';
 
 ReactDOM.render(
   <Router>
     <App />
-    <Route exact path="/" component={Home}/>
+
 
   </Router>,
   document.getElementById('root')
